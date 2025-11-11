93350638e612760874d8ea7a3e5c5f2de2656e66
Casper <hicasper@users.noreply.github.com>
2025-11-11 13:09:09 +0800

    feat: add openappfilter

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -72,6 +72,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
