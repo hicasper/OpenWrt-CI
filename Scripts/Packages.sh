3c3864f357365ac85518a60396c86bffe93cb26b
Casper <hicasper@users.noreply.github.com>
2026-07-27 13:49:05 +0800

    feat: add packages

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -82,6 +82,7 @@ UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac sing-box luci-app-homeproxy luci-app-timewol luci-app-wolplus luci-app-wolultra"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master"
+UPDATE_PACKAGE "luci-app-adguardhome" "sirpdboy/luci-app-adguardhome" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
